8da05e768f9aedcae90dd3fba81cf6b6b665100a
JD Benito <jd@hybridforge.com>
2017-09-15 16:21:25 -0600

    FOL-145 #time 2h Implemented the samples for the vendor create

--- src/main/resources/db/migration/V1__Create_acquisition_DB.sql
+++ src/main/resources/db/migration/V1__Create_acquisition_DB.sql
@@ -1,266 +1,324 @@
-/*
- * Language
+/**
+ * The following tables all relate to Contacts. These may need to be decoupled from the Vendors module
+ * and encapsulated into its own Contacts module.
  */
-create table if not exists Language (
-    id serial primary key,
-    code varchar(25) not null,
-    description varchar(255) not null
+
+create table "phone_number" (
+  "id" bigserial primary key,
+  "country_code" varchar(25),
+  "area_code" varchar(25),
+  "phone_number" varchar(25) not null
 );
 
-/*
- * Currency
- */
-create table if not exists Currency (
-    id serial primary key,
-    code varchar(25) not null,
-    description varchar(255) not null
+create table "email" (
+  "id" bigserial primary key,
+  "email" varchar(255) not null
 );
 
-/*
- * VendorStatus
- */
-create table if not exists Vendor_Status (
-    id serial primary key,
-    code varchar(25) not null,
-    description varchar(255) not null
+create table "address" (
+  "id" bigserial primary key,
+  "address_line_1" varchar(255),
+  "address_line_2" varchar(255),
+  "city" varchar(100),
+  "province" varchar(100),
+  "postal_code" varchar(100),
+  "country" varchar(100)
 );
 
-/*
- * InterfaceStatus
- */
-create table if not exists Interface_Status (
-    id serial primary key,
-    code varchar(25) not null,
-    description varchar(255) not null
+create table "contact_person" (
+  "id" bigserial primary key,
+  "prefix" varchar(100),
+  "first_name" varchar(255) not null,
+  "last_name" varchar(255),
+  "language" varchar(255),
+  "notes" text,
+  "phone_number_id" integer references "phone_number",
+  "email_id" integer references "email",
+  "address_id" integer references "address"
 );
 
-/*
- * Delivery Method
- */
-create table if not exists Delivery_Method (
-    id serial primary key,
-    code varchar(25) not null,
-    description varchar(255) not null
-);
 
-/*
- * Statistics Format
- */
-create table if not exists Statistics_Format (
-    id serial primary key,
-    code varchar(25) not null,
-    description varchar(255) not null
-);
 
 /**
- * Statistics
+ * The following tables all relate to the Vendor-module
  */
-create table if not exists Statistics (
-    id serial primary key,
-    available boolean,
-    locally_stored varchar(50),
-    online_location varchar(255),
-    note text,
-    delivery_method_id integer references Delivery_Method,
-    format_id integer references Statistics_Format
+
+create table "category" (
+  "id" bigserial primary key,
+  "value" varchar(100) not null
 );
 
-/**
- * Admin Info
- */
-create table if not exists Admin_Info (
-    id serial primary key,
-    documentation text,
-    integration_note varchar(255),
-    hardware_req_internal varchar(255),
-    hardware_req_public varchar(255),
-    maintenance_win varchar(255),
-    software_req_public varchar(255),
-    software_req_internal varchar(255),
-    uri varchar(255)
+insert into "category" ("value") values
+('Returns'), ('Payments'), ('Customer Service'), ('Shipments');
+
+create table "contact_category" (
+  "id" bigserial primary key,
+  "value" varchar(100) not null
 );
 
-create table if not exists Interface_Access (
-    id serial primary key,
-    auth_method varchar(255),
-    instructions text,
-    uri varchar(255),
-    ip_send_method varchar(255),
-    user_id varchar(255)
+insert into "contact_category" ("value") values
+('Accounting'), ('Books'), ('Customer Service'), ('Databases'), ('Ebooks'), ('Econtent'),
+('General'), ('Journals'), ('Licenses'), ('Primary'), ('Sales'), ('Serials');
+
+create table "edi_type" (
+  "id" bigserial primary key,
+  "code" varchar(25) not null,
+  "description" varchar(100)
 );
 
-/**
- * Contact Information
- */
-create table if not exists Contact_Info (
-    id serial primary key
+insert into "edi_type" ("code") values
+('014/EAN'), ('31B/US-SAN'), ('091/Vendor-assigned'), ('092/Customer-assigned');
+
+create table "payment_method" (
+  "id" bigserial primary key,
+  "code" varchar(25) not null,
+  "description" varchar(100)
 );
 
-/*
- * Interface
- */
-create table if not exists Interface (
-    id serial primary key,
-    "name" varchar(255) not null,
-    description varchar(255) not null,
-    access_info_id integer references Interface_Access,
-    admin_info_id integer references Admin_Info,
-    interface_contact_id integer references Contact_Info,
-    interface_status_id integer references Interface_Status,
-    statistics_id integer references Statistics
+insert into "payment_method" ("code") values
+('EFT'), ('Bank Draft'), ('Paper Check'), ('Credit Card/P-Card'), ('Deposit Account'), ('Cash');
+
+create table "statistics_format" (
+  "id" bigserial primary key,
+  "code" varchar(25) not null,
+  "description" varchar(100)
 );
 
-create table if not exists Interface_Note (
-    id serial primary key,
-    note_text text,
-    interface_id integer references Interface
+insert into "statistics_format" ("code") values
+('Delimited'), ('Excel'), ('CSV'), ('PDF'), ('ASCII'), ('HTML'), ('Other');
+
+create table "delivery_method" (
+  "id" bigserial primary key,
+  "code" varchar(25) not null,
+  "description" varchar(100)
 );
 
-/*
- * EDI Type
- * 
- * Models the supported EDI protocols
- */
-create table if not exists Edi_Type (
-    id serial primary key,
-    code varchar(25) not null,
-    description varchar(255) not null
+insert into "delivery_method" ("code") values
+('Online'), ('FTP'), ('Email');
+
+create table "ftp_format" (
+  "id" bigserial primary key,
+  "code" varchar(25) not null,
+  "description" varchar(100)
 );
 
+insert into "ftp_format" ("code") values
+('SFTP'), ('FTP');
 
-/*
- * FTP Mode
- */
-create table if not exists Ftp_Mode (
-    id serial primary key,
-    code varchar(25) not null,
-    description varchar(255) not null
+create table "ftp_mode" (
+  "id" bigserial primary key,
+  "code" varchar(25) not null,
+  "description" varchar(100)
 );
 
-/*
- * EDI Naming Convention
- */
-create table if not exists Edi_Naming_Convention (
-    id serial primary key,
-    code varchar(25) not null,
-    description varchar(255) not null
+insert into "ftp_mode" ("code") values
+('ASCII'), ('BINARY');
+
+create table "vendor_status" (
+  "id" bigserial primary key,
+  "code" varchar(25) not null,
+  "description" varchar(100)
 );
 
-/*
- * Send Command
- */
-create table if not exists Send_Command (
-    id serial primary key,
-    code varchar(25) not null,
-    description varchar(255) not null
+insert into "vendor_status" ("code") values
+('Active'), ('Pending'), ('Inactive');
+
+create table "lib_ven_acct_status" (
+  "id" bigserial primary key,
+  "code" varchar(25) not null,
+  "description" varchar(100)
 );
 
-/*
- * Vendor EDI Format
- */
-create table if not exists Edi_Format (
-    id serial primary key,
-    code varchar(25) not null,
-    description varchar(255) not null
+insert into "lib_ven_acct_status" ("code") values
+('Active'), ('Pending'), ('Inactive');
+
+
+create table "edi_info" (
+  "id" bigserial primary key,
+  "vendor_edi_code" varchar(100),
+  "vendor_edi_type" varchar(100),           -- This pulls values from the "code" field of "edi_type"
+  "lib_edi_code" varchar(100),
+  "lib_edi_type" varchar(100),              -- This pulls values from the "code" field of "edi_type"
+  "edi_naming_convention" varchar(255),
+  "prorate_tax" boolean,
+  "prorate_fees" boolean,
+  "send_acct_num" boolean,
+  "support_order" boolean,
+  "support_invoice" boolean,
+  "notes" text,
+  "ftp_format" varchar(255),                -- i.e. SFTP, FTP
+  "ftp_mode" varchar(255),                  -- i.e. ASCII, BINARY
+  "ftp_conn_mode" varchar(255),             -- i.e. ACTIVE, PASSIVE
+  "ftp_port" varchar(10),                   -- Defaults: 21 (FTP)/22 (SFTP)
+  "server_address" varchar(10),
+  "username" varchar(50),
+  "password" varchar(50),
+  "order_directory" varchar(255),
+  "invoice_directory" varchar(255),
+  "send_to_emails" text,                    -- comma-separated string
+  "notify_all_edi" bool,
+  "notify_invoice_only" bool,
+  "notify_error_only" bool
 );
 
-/*
- * EDI Info
- */
-create table if not exists Edi_Info (
-    id serial primary key,
-    additional_order_no boolean default false,
-    allow_navigation boolean default false,
-    code varchar(25),
-    invoices boolean default false,
-    po_lines boolean default false,
-    include_fund_code boolean default false,
-    do_not_prorate boolean default false,
-    ftp_description varchar(255),
-    ftp_username varchar(255),
-    input_path varchar(512),
-    output_path varchar(512),
-    passive_mode boolean default false,
-    port_no integer,
-    secured_ftp boolean default false,
-    server_name varchar(255),
-    tenant_specific_code varchar(255),
-    edi_type_id integer references Edi_Type,
-    ftp_mode_id integer references Ftp_Mode,
-    edi_naming_convention_id integer references Edi_Naming_Convention,
-    send_command_id integer references Send_Command,
-    edi_format_id integer references Edi_Format
+create table "vendor" (
+  "id" bigserial primary key,
+  "name" varchar(255) not null,
+  "code" varchar(50),
+  "vendor_status" varchar(50),                   -- Active, Pending, Inactive
+  "language" varchar(255),
+  "erp_code" varchar(50),
+  "payment_method" varchar(100),                 -- e.g. EFT, Bank Draft, Paper Check, Credit Card/P-Card, Deposit Account, Cash
+  "access_provider" bool,
+  "governmental" bool,
+  "licensor" bool,
+  "material_supplier" bool,
+  "claiming_interval" int,
+  "discount_percent" decimal,
+  "expected_activation_interval" int,
+  "expected_invoice_interval" int,
+  "renewal_activation_interval" int,
+  "subscription_interval" int,
+  "liable_for_vat" bool,
+  "tax_id" varchar(50),
+  "tax_percentage" decimal,
+  "edi_info_id" integer references "edi_info"
 );
 
-/*
- * LibVenAcctStatus
- */
-create table if not exists Lib_Ven_Acct_Status (
-    id serial primary key,
-    code varchar(25) not null,
-    description varchar(255) not null
+create table "vendor_name" (
+  "id" bigserial primary key,
+  "value" varchar(255) not null,
+  "description" varchar(100),
+  "vendor_id" integer references "vendor"
 );
 
-/*
- * Library
- */
-create table if not exists Library (
-    id serial primary key,
-    code varchar(25) not null,
-    description varchar(255) not null
+create table "vendor_currency" (
+  "id" bigserial primary key,
+  "currency" varchar(255) not null,
+  "vendor_id" integer references "vendor"
 );
 
-/*
- * Payment Method
- */
-create table if not exists Payment_Method (
-    id serial primary key,
-    code varchar(25) not null,
-    description varchar(255) not null
+create table "vendor_interface" (
+  "id" bigserial primary key,
+  "name" varchar(255),
+  "uri" varchar(255) not null,
+  "username" varchar(100),
+  "password" varchar(255),                      -- Would this need to be encrypted?
+  "notes" text,
+  "available" boolean,
+  "delivery_method" varchar(255),               -- e.g. Online, FTP, Email
+  "statistics_format" varchar(255),             -- e.g. Delimited, Excel, CSV, PDF, ASCII, HTML, Other
+  "locally_stored" varchar(255),
+  "online_location" varchar(255),
+  "statistics_notes" text,
+  "vendor_id" integer references "vendor"
 );
 
-/**
- * Vendor
- */
-create table if not exists Vendor (
-    id serial primary key,
-    access_provider boolean default false,
-    claiming_interval integer,
-    code varchar(50),
-    discount_percent decimal,
-    expected_activation_interval integer,
-    expected_invoice_interval integer,
-    financial_sys_code varchar(255),
-    governmental boolean default false,
-    liable_for_vat boolean default false,
-    licensor boolean default false,
-    material_supplier boolean default false,
-    "name" varchar(255) not null,
-    national_tax_id varchar(50),
-    renewal_activation_interval varchar(50),
-    subscription_interval varchar(50),
-    tax_percentage decimal,
-    contact_info_id integer references Contact_Info,
-    currency_id integer references Currency,
-    edi_info_id integer references Edi_Info,
-    interface_id integer references Interface,
-    language_id integer references Language,
-    vendor_status_id integer references Vendor_Status
+create table "agreement" (
+  "id" bigserial primary key,
+  "discount" decimal,
+  "name" varchar(255),
+  "notes" text,
+  "reference_url" varchar(255) not null,
+  "vendor_id" integer references "vendor"
 );
 
-/*
- * Library Vendor Account
- */
-create table if not exists Library_Vendor_Account (
-    id serial primary key,
-    account_no varchar(25),
-    discount_percent decimal,
-    description varchar(255),
-    note text,
-    contact_info_id integer references Contact_Info,
-    edi_info_id integer references Edi_Info,
-    library_id integer references Library,
-    payment_method_id integer references Payment_Method,
-    status_id integer references Lib_Ven_Acct_Status,
-    vendor_id integer references Vendor
+create table "library_vendor_acct" (
+  "id" bigserial primary key,
+  "name" varchar(255),
+  "payment_method" varchar(100),                 -- e.g. EFT, Bank Draft, Paper Check, Credit Card/P-Card, Deposit Account, Cash
+  "account_no" varchar(100) not null,
+  "lib_ven_acct_status" varchar(100) not null,   -- Active, Inactive, Pending
+  "description" varchar (255),
+  "contact_info" varchar(255),
+  "apsystem_no" varchar(100),                    -- Account Payable System No.
+  "notes" text,
+  "library_code" varchar(100),
+  "library_edi_code" varchar(100),
+  "vendor_id" integer references "vendor"
+);
+
+create table "note" (
+  "id" bigserial primary key,
+  "description" varchar(255) not null,
+  "timestamp" timestamp,
+  "user_id" varchar(255),                         -- Weak reference to a user ID
+  "vendor_id" integer references "vendor"
+);
+
+create table "job" (
+  "id" bigserial primary key,
+  "is_scheduled" boolean,
+  "start_date" timestamp,
+  "time" time,
+  "is_monday" boolean,
+  "is_tuesday" boolean,
+  "is_wednesday" boolean,
+  "is_thursday" boolean,
+  "is_friday" boolean,
+  "is_saturday" boolean,
+  "is_sunday" boolean,
+  "scheduling_notes" text,
+  "vendor_id" integer references "vendor"
+);
+
+create table "vendor_address" (
+  "id" bigserial primary key,
+  "language" varchar(255),
+  "address" varchar(255),                        -- Weak reference to an "address"
+  "vendor_id" integer references "vendor"
+);
+
+create table "vendor_address_categories" (
+  "vendor_address_id" integer references "vendor_address",
+  "category_id" integer references "category"
+);
+
+create table "vendor_phone" (
+  "id" bigserial primary key,
+  "language" varchar(255),
+  "phone_number" varchar(255),                   -- Weak reference to a "phone_number"
+  "vendor_id" integer references "vendor"
+);
+
+create table "vendor_phone_categories" (
+  "vendor_phone_id" integer references "vendor_phone",
+  "category_id" integer references "category"
+);
+
+create table "vendor_email" (
+  "id" bigserial primary key,
+  "language" varchar(255),
+  "email" varchar(255),                          -- Weak reference to an "email"
+  "vendor_id" integer references "vendor"
+);
+
+create table "vendor_email_categories" (
+  "vendor_email_id" integer references "vendor_email",
+  "category_id" integer references "category"
+);
+
+create table "vendor_contact" (
+  "id" bigserial primary key,
+  "language" varchar(255),
+  "contact_person_id" varchar(255),             -- Weak reference to a "contact_person_id"
+  "vendor_id" integer references "vendor"
+);
+
+create table "vendor_contact_categories" (
+  "vendor_contact_id" integer references "vendor_contact",
+  "category_id" integer references "contact_category"
+);
+
+create table "vendor_url" (
+  "id" bigserial primary key,
+  "value" varchar(2000) not null,
+  "language" varchar(255),
+  "notes" text,
+  "vendor_id" integer references "vendor"
+);
+
+create table "vendor_url_categories" (
+  "vendor_url_id" integer references "vendor_url",
+  "category_id" integer references "category"
 );
