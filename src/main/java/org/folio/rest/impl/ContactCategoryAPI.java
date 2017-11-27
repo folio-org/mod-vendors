709364ffd8ae5278a31623678e5ddea4ce23cfb0
JD Benito <jd@hybridforge.com>
2017-11-27 11:01:23 -0700

    FOL-214 Added API implementations for contact_category

--- src/main/java/org/folio/rest/impl/ContactCategoryAPI.java
+++ src/main/java/org/folio/rest/impl/ContactCategoryAPI.java
@@ -10,17 +10,20 @@ import org.folio.rest.RestVerticle;
 import org.folio.rest.jaxrs.model.Category;
 import org.folio.rest.jaxrs.model.ContactCategoryCollection;
 import org.folio.rest.jaxrs.resource.ContactCategoryResource;
+import org.folio.rest.persist.Criteria.Criteria;
 import org.folio.rest.persist.Criteria.Criterion;
 import org.folio.rest.persist.Criteria.Limit;
 import org.folio.rest.persist.Criteria.Offset;
 import org.folio.rest.persist.PostgresClient;
 import org.folio.rest.tools.messages.MessageConsts;
 import org.folio.rest.tools.messages.Messages;
+import org.folio.rest.tools.utils.OutStream;
 import org.folio.rest.tools.utils.TenantTool;
 
 import javax.ws.rs.core.Response;
 import java.util.List;
 import java.util.Map;
+import java.util.UUID;
 
 public class ContactCategoryAPI implements ContactCategoryResource {
   private static final String CONTACT_CATEGORY_TABLE = "contact_category";
@@ -115,21 +118,182 @@ public class ContactCategoryAPI implements ContactCategoryResource {
 
   @Override
   public void postContactCategory(String lang, Category entity, Map<String, String> okapiHeaders, Handler<AsyncResult<Response>> asyncResultHandler, Context vertxContext) throws Exception {
+    vertxContext.runOnContext(v -> {
 
+      try {
+        String id = UUID.randomUUID().toString();
+        if(entity.getId() == null){
+          entity.setId(id);
+        }
+        else{
+          id = entity.getId();
+        }
+
+        String tenantId = TenantTool.calculateTenantId( okapiHeaders.get(RestVerticle.OKAPI_HEADER_TENANT) );
+        PostgresClient.getInstance(vertxContext.owner(), tenantId).save(
+          CONTACT_CATEGORY_TABLE, id, entity,
+          reply -> {
+            try {
+              if (reply.succeeded()) {
+                String persistenceId = reply.result();
+                entity.setId(persistenceId);
+                OutStream stream = new OutStream();
+                stream.setData(entity);
+
+                Response response = PostContactCategoryResponse.
+                  withJsonCreated(CONTACT_CATEGORY_LOCATION_PREFIX + persistenceId, stream);
+                respond(asyncResultHandler, response);
+              }
+              else {
+                log.error(reply.cause().getMessage(), reply.cause());
+                Response response = PostContactCategoryResponse.withPlainInternalServerError(reply.cause().getMessage());
+                respond(asyncResultHandler, response);
+              }
+            }
+            catch (Exception e) {
+              log.error(e.getMessage(), e);
+
+              Response response = PostContactCategoryResponse.withPlainInternalServerError(e.getMessage());
+              respond(asyncResultHandler, response);
+            }
+
+          }
+        );
+      }
+      catch (Exception e) {
+        log.error(e.getMessage(), e);
+
+        String errMsg = messages.getMessage(lang, MessageConsts.InternalServerError);
+        Response response = PostContactCategoryResponse.withPlainInternalServerError(errMsg);
+        respond(asyncResultHandler, response);
+      }
+
+    });
   }
 
   @Override
   public void getContactCategoryByCategoryId(String categoryId, String lang, Map<String, String> okapiHeaders, Handler<AsyncResult<Response>> asyncResultHandler, Context vertxContext) throws Exception {
+    vertxContext.runOnContext(v -> {
+      try {
+        String tenantId = TenantTool.calculateTenantId( okapiHeaders.get(RestVerticle.OKAPI_HEADER_TENANT) );
 
+        String idArgument = String.format("'%s'", categoryId);
+        Criterion c = new Criterion(
+          new Criteria().addField(idFieldName).setJSONB(false).setOperation("=").setValue(idArgument));
+
+        PostgresClient.getInstance(vertxContext.owner(), tenantId).get(CONTACT_CATEGORY_TABLE, Category.class, c, true,
+          reply -> {
+            try {
+              if (reply.succeeded()) {
+                @SuppressWarnings("unchecked")
+                List<Category> results = (List<Category>) reply.result().getResults();
+                if (results.isEmpty()) {
+                  asyncResultHandler.handle(Future.succeededFuture(ContactCategoryAPI.GetContactCategoryByCategoryIdResponse
+                    .withPlainNotFound(categoryId)));
+                }
+                else{
+                  asyncResultHandler.handle(Future.succeededFuture(ContactCategoryAPI.GetContactCategoryByCategoryIdResponse
+                    .withJsonOK(results.get(0))));
+                }
+              }
+              else{
+                log.error(reply.cause().getMessage(), reply.cause());
+                if (isInvalidUUID(reply.cause().getMessage())) {
+                  asyncResultHandler.handle(Future.succeededFuture(ContactCategoryAPI.GetContactCategoryByCategoryIdResponse
+                    .withPlainNotFound(categoryId)));
+                }
+                else{
+                  asyncResultHandler.handle(Future.succeededFuture(ContactCategoryAPI.GetContactCategoryByCategoryIdResponse
+                    .withPlainInternalServerError(messages.getMessage(lang, MessageConsts.InternalServerError))));
+                }
+              }
+            } catch (Exception e) {
+              log.error(e.getMessage(), e);
+              asyncResultHandler.handle(Future.succeededFuture(ContactCategoryAPI.GetContactCategoryByCategoryIdResponse
+                .withPlainInternalServerError(messages.getMessage(lang, MessageConsts.InternalServerError))));
+            }
+          });
+      } catch (Exception e) {
+        log.error(e.getMessage(), e);
+        asyncResultHandler.handle(Future.succeededFuture(ContactCategoryAPI.GetContactCategoryByCategoryIdResponse
+          .withPlainInternalServerError(messages.getMessage(lang, MessageConsts.InternalServerError))));
+      }
+    });
   }
 
   @Override
   public void deleteContactCategoryByCategoryId(String categoryId, String lang, Map<String, String> okapiHeaders, Handler<AsyncResult<Response>> asyncResultHandler, Context vertxContext) throws Exception {
+    String tenantId = TenantTool.tenantId(okapiHeaders);
 
+    try {
+      vertxContext.runOnContext(v -> {
+        PostgresClient postgresClient = PostgresClient.getInstance(
+          vertxContext.owner(), TenantTool.calculateTenantId(tenantId));
+
+        try {
+          postgresClient.delete(CONTACT_CATEGORY_TABLE, categoryId, reply -> {
+            if (reply.succeeded()) {
+              asyncResultHandler.handle(Future.succeededFuture(
+                ContactCategoryAPI.DeleteContactCategoryByCategoryIdResponse.noContent()
+                  .build()));
+            } else {
+              asyncResultHandler.handle(Future.succeededFuture(
+                ContactCategoryAPI.DeleteContactCategoryByCategoryIdResponse.
+                  withPlainInternalServerError(reply.cause().getMessage())));
+            }
+          });
+        } catch (Exception e) {
+          asyncResultHandler.handle(Future.succeededFuture(
+            ContactCategoryAPI.DeleteContactCategoryByCategoryIdResponse.
+              withPlainInternalServerError(e.getMessage())));
+        }
+      });
+    }
+    catch(Exception e) {
+      asyncResultHandler.handle(Future.succeededFuture(
+        ContactCategoryAPI.DeleteContactCategoryByCategoryIdResponse.
+          withPlainInternalServerError(e.getMessage())));
+    }
   }
 
   @Override
   public void putContactCategoryByCategoryId(String categoryId, String lang, Category entity, Map<String, String> okapiHeaders, Handler<AsyncResult<Response>> asyncResultHandler, Context vertxContext) throws Exception {
-
+    vertxContext.runOnContext(v -> {
+      String tenantId = TenantTool.calculateTenantId( okapiHeaders.get(RestVerticle.OKAPI_HEADER_TENANT) );
+      try {
+        if(entity.getId() == null){
+          entity.setId(categoryId);
+        }
+        PostgresClient.getInstance(vertxContext.owner(), tenantId).update(
+          CONTACT_CATEGORY_TABLE, entity, categoryId,
+          reply -> {
+            try {
+              if(reply.succeeded()){
+                if (reply.result().getUpdated() == 0) {
+                  asyncResultHandler.handle(Future.succeededFuture(ContactCategoryAPI.PutContactCategoryByCategoryIdResponse
+                    .withPlainNotFound(messages.getMessage(lang, MessageConsts.NoRecordsUpdated))));
+                }
+                else{
+                  asyncResultHandler.handle(Future.succeededFuture(ContactCategoryAPI.PutContactCategoryByCategoryIdResponse
+                    .withNoContent()));
+                }
+              }
+              else{
+                log.error(reply.cause().getMessage());
+                asyncResultHandler.handle(Future.succeededFuture(ContactCategoryAPI.PutContactCategoryByCategoryIdResponse
+                  .withPlainInternalServerError(messages.getMessage(lang, MessageConsts.InternalServerError))));
+              }
+            } catch (Exception e) {
+              log.error(e.getMessage(), e);
+              asyncResultHandler.handle(Future.succeededFuture(ContactCategoryAPI.PutContactCategoryByCategoryIdResponse
+                .withPlainInternalServerError(messages.getMessage(lang, MessageConsts.InternalServerError))));
+            }
+          });
+      } catch (Exception e) {
+        log.error(e.getMessage(), e);
+        asyncResultHandler.handle(Future.succeededFuture(ContactCategoryAPI.PutContactCategoryByCategoryIdResponse
+          .withPlainInternalServerError(messages.getMessage(lang, MessageConsts.InternalServerError))));
+      }
+    });
   }
 }
